18669023360544cc5767bdae1a6f90171cae8f28
Sunil Kumar Yadav <123sunilkr@gmail.com>
2022-10-03 23:27:38 +0530

    intial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # hr-coding-challenge
+
+## Warm-up Challenges
+
+1. Sales by MAtch
+2. Counting Valleys
+3. Jumping on the Couds
+4. Repeated String
+
+## Array
+
+1. Two number sum
---
+++ docs/coding.js
@@ -0,0 +1,15 @@
+
+
+const data = {
+    'Warm -up Challenges': [
+        { 'Sales by Match': ['', '', ''] },
+        { 'Counting Valleys': ['', '', ''] },
+        { 'Jumping on the Couds': ['', '', ''] },
+        { 'Repeated String': ['', '', ''] }
+    ],
+    'Array': [
+        { '2D Array - DS': ['', '', 'https://www.hackerrank.com/challenges/2d-array/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays'] },
+        { 'Arrays: Left Rotation': ['', '', 'https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays'] },
+        { 'Two number sum': [] }
+    ]
+}
