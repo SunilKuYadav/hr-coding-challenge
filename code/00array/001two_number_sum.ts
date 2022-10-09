1212ae0b42377b1c3f358638c87da478aed05ed0
Sunil Kumar Yadav <123sunilkr@gmail.com>
2022-10-09 17:26:29 +0530

    update

---
+++ code/00array/001two_number_sum.ts
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums: number[], target: number): number[] {
+  for (let left = 0; left < nums.length; left++) {
+    for (let right = left + 1; right < nums.length; right++) {
+      if (nums[left] + nums[right] === target) {
+        return [left, right];
+      }
+    }
+  }
+  return [-1, -1];
+};
