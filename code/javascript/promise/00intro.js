d66f0dbde3f31a08b059dadc99fbb603e7e73a53
Sunil Kumar Yadav <mobcoderid-105@Mobcoders-Mac-mini-5.local>
2022-11-28 14:27:43 +0530

    Add: intro of promise

---
+++ code/javascript/promise/00intro.js
@@ -0,0 +1,19 @@
+// Promise
+
+// The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting values.
+// It has three state
+// 1. Pending
+// 2. Fulfilld
+// 3. Rejected
+
+// Chained Promise
+// 1. Promise.prototype.then() (response(), reject())
+// 2. Promise.prototype.catch() => then() without response callback()
+// 3. Promise.prototype.finally()
+
+// Promise Concurrency
+// 1. Promise.all()
+// 2. Promise.allSettled()
+// 3. Promise.any()
+// 4. Promise.race()
+// 5. Promise.all()
