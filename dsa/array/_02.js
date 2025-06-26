d1f583d1cdf2f9062ec7b61d53b4dad1d585c30b
sunil kumar yadav <123sunilkr@gmail.com>
2025-06-27 00:16:59 +0530

    FEAT: Two Number Sum

---
+++ dsa/array/_02.js
@@ -0,0 +1,12 @@
+export const _02 = {
+    problem: 'Two Number Sum',
+    inputs: [
+        [[5, 3, -4, 8, 11, 1, -1, 6], 10],
+        [[-4, -1, 1, 3, 5, 6, 8, 11], 13]
+    ],
+    outputs: [
+        [-1, 11],
+        [5, 8]
+    ],
+    solution: []
+}
---
+++ dsa/array/_02_01.js
@@ -0,0 +1,72 @@
+import { _02 } from './_02.js'
+
+const inputs = _02.inputs
+const outputs = _02.outputs
+
+const makeDuplicate = input => JSON.parse(JSON.stringify(input))
+
+
+// Dum way - Time O(n^2) | Space O(1)
+const solution_01 = (arr, targetSum) => {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === targetSum) {
+                // sorting is optional
+                return [arr[i], arr[j]].sort((a, b) => a - b);
+            }
+        }
+    }
+    return 'Not Found'
+}
+
+// Using hash table - Time O(n) | Space O(n)
+const solution_02 = (arr, targetSum) => {
+    const hash = []
+    for (let i = 0; i < arr.length; i++) {
+        const potentialMatch = targetSum - arr[i]
+        if (hash.includes(potentialMatch)) {
+            return [arr[i], targetSum - arr[i]].sort((a, b) => a - b)
+        } else {
+            hash.push(arr[i])
+        }
+    }
+    return 'Not Found'
+}
+
+// Using two pointers - Time O(n) | space O(1)
+// Condition - if arr is sorted
+const solution_03 = (arr, targetSum) => {
+    let left = 0, right = arr.length - 1
+    for (let i = 0; i < arr.length; i++) {
+        const potentialMatch = arr[left] + arr[right]
+        if (potentialMatch === targetSum) {
+            return [arr[left], arr[right]].sort((a, b) => a - b)
+        } else if(potentialMatch > targetSum) {
+            right--
+        } else if (potentialMatch < targetSum) {
+            left++
+        }
+    }
+    return 'Not Found'
+}
+
+const runFile = (solution, input, output) => {
+    const getOutput = solution(input[0], input[1])
+
+    const isPass = JSON.stringify(getOutput) === JSON.stringify(output)
+    if (isPass) {
+        console.log('Pass')
+    } else {
+        console.log('Input', input)
+        console.log('Your Output', getOutput)
+        console.log('Expected Output', output)
+        console.error( 'Fail')
+    }
+}
+
+const solutions = [solution_01, solution_02, solution_03]
+inputs.forEach((input, i) => {
+    solutions.forEach(solution => {
+        runFile(solution, input, outputs[i])
+    })
+})
