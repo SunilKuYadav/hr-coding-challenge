dd407631c6f0a2a538460f62fd9c006815e93c68
sunil kumar yadav <123sunilkr@gmail.com>
2025-06-28 13:04:59 +0530

    FEAT: Longest pick

---
+++ dsa/array/_07.js
@@ -0,0 +1,9 @@
+export const _07 = {
+    problem: 'Longest pick',
+    inputs: [
+        [1, 2, 3, 3, 4, 0, 10, 6, 5, 0, -1, -3, 2, 3],
+        [1, 2, 3, 3, 4, 0, 10, 3]
+    ],
+    outputs: [7, 3],
+    solution: []
+}
---
+++ dsa/array/_07_01.js
@@ -0,0 +1,58 @@
+import {_07} from "./_07.js";
+import {deepEqual, makeDeepCopy} from "../helperUtils.js";
+
+const inputs = _07.inputs
+const outputs = _07.outputs
+
+// Iterative way - Time O(n) | Space O(1)
+const solution_01 = (arr) => {
+    let longestPickLength = 0
+    let i = 1
+
+    while (i < arr.length - 1) {
+        const isPick = arr[i - 1] < arr[i] && arr[i] > arr[i + 1]
+        if (!isPick) {
+            i++
+            continue
+        }
+
+        let leftIndex = i - 2
+        let rightIndex = i + 2
+
+        // check left side
+        while (leftIndex >= 0 && arr[leftIndex] < arr[leftIndex + 1]) {
+            leftIndex--
+        }
+        // check right side
+        while (rightIndex < arr.length && arr[rightIndex] < arr[rightIndex - 1]) {
+            rightIndex++
+        }
+
+        let currentPickLength = rightIndex - leftIndex - 1
+        longestPickLength = Math.max(longestPickLength, currentPickLength)
+        i = rightIndex
+    }
+    return longestPickLength
+}
+
+
+const runFile = (solution, input, output) => {
+    const getOutput = solution(input)
+
+    const isPass = deepEqual(getOutput, output)
+    if (isPass) {
+        console.log('Pass')
+    } else {
+        console.log('Input', input)
+        console.log('Your Output', getOutput)
+        console.log('Expected Output', output)
+        console.error( 'Fail')
+    }
+}
+
+const solutions = [solution_01]
+inputs.forEach((input, i) => {
+    solutions.forEach(solution => {
+        runFile(solution, makeDeepCopy(input), makeDeepCopy(outputs[i]))
+    })
+})
