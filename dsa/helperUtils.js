3581e5f3af3b435472d4913c1b6e0b288f3c00da
sunil kumar yadav <123sunilkr@gmail.com>
2025-07-02 01:53:56 +0530

    FEAT: helper functions

--- dsa/helperUtils.js
+++ dsa/helperUtils.js
@@ -18,7 +18,108 @@ const deepEqual = (a, b) => {
     return false;
 }
 
+
+class SinglyNode {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+const createSinglyList = (data) => {
+    if (data.length === 0) return null
+
+    let head = new SinglyNode(data[0])
+    let current = head
+
+    for (let i = 1; i < data.length; i++) {
+        current.next = new SinglyNode(data[i])
+        current = current.next
+    }
+
+    return  head
+
+}
+const areSinglyListIsEqual = (head1, head2) =>{
+    let current1 = head1
+    let current2 = head2
+
+    while (current1 !== null && current2 !== null) {
+        if (current1.data !== current2.data) {
+            return false
+        }
+        current1 = current1.next
+        current2 = current2.next
+    }
+    return current1 === null && current2 === null;
+}
+const printSinglyList = (head) => {
+    let current = head;
+    const values = [];
+
+    while (current !== null) {
+        values.push(current.data);
+        current = current.next;
+    }
+
+    console.log(values.join(" -> "));
+}
+const isValidSinglyList = (head) => {
+    let current = head;
+    const seenNode = new Set()
+
+    while (current !== null) {
+        if (typeof current !== 'object' || !('data' in current) ||
+            !('next' in current)) {
+            return null
+        }
+
+        if (seenNode.has(current)) {
+            return null
+        }
+
+        seenNode.add(current)
+        current = current.next;
+    }
+    return head
+}
+const createCyclicSinglyList = (data, position) => {
+    if (data.length === 0) return {head: null, cyclicNode: null}
+
+    const head = new SinglyNode(data[0])
+    let current = head
+    let cyclicNode = null
+
+    for (let i = 1; i < data.length; i++) {
+        const newNode = new SinglyNode(data[i])
+        current.next = newNode
+        current = newNode
+
+        if(i === position) {
+            cyclicNode = newNode
+        }
+    }
+
+    if (position === 0) {
+        cyclicNode = head
+    }
+
+    if (position >= 0) {
+        current.next = cyclicNode
+    }
+
+    return {head, cyclicNode}
+}
+const LinkedList = {
+    SinglyNode,
+    isValidSinglyList,
+    printSinglyList,
+    createSinglyList,
+    areSinglyListIsEqual,
+    createCyclicSinglyList
+}
+
 export {
     makeDeepCopy,
-    deepEqual
+    deepEqual,
+    LinkedList
 }
