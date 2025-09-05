11f103681dce5126d1142276bcd5f8a7306c361c
sunil kumar yadav <123sunilkr@gmail.com>
2025-09-06 01:41:54 +0530

    JS:  UML Diagram

--- javascript/lld.js
+++ javascript/lld.js
@@ -109,3 +109,80 @@
 // Method Overriding:
 // 1. Override the method of the parent class
 // 2. Example: Car
+
+
+// UML Diagram
+// 1. Structural Diagram
+// 2. Behavioral Diagram
+
+// Structural Diagram
+// 1. Class Diagram <<<<====
+// 2. Object Diagram
+// 3. Component Diagram
+// 4. Deployment Diagram
+// 5. Composite Structure Diagram
+// 6. Package Diagram
+// 7. Profile Diagram
+// 8. UML Diagram
+
+// Behavioral Diagram
+// 1. Activity Diagram
+// 2. State Machine Diagram
+// 3. Sequence Diagram <<<<====
+// 4. Communication Diagram
+// 5. Interaction Overview Diagram
+
+
+// class representation
+// [
+// class name
+//     Car
+
+// properties
+//     - brand: string
+//     - modal: string
+//     - isEnginOn: boolean
+//     - currentSpeed: number
+//     - tyre: string
+
+// methods
+//     + startEngine(): void
+//     + accelerate(): void
+//     + brake(): void
+//     + stopEngine(): void
+// ]
+
+// + => public modifier
+// - => private modifier
+// # => protected modifier
+
+// association
+// 1. class association
+// 2. object association
+
+// class association
+// 1. aggregation
+// 2. composition
+// eg. Inheritance (Is-a relationship)
+
+// object association
+// 1. association
+// 2. aggregation
+// 3. composition (has-a relationship)
+
+// Sequence Diagram - Behavioral Diagram
+// Object - with square box
+// Lifeline - with dashed line
+// Activation bar - with rectangle box
+// Message - with arrow
+// async message
+// sync message
+// lost message
+// found message
+
+// Aggregation - with diamond shape
+// Composition - with filled diamond shape
+
+// alt: if else
+// option: if
+// loop: for
