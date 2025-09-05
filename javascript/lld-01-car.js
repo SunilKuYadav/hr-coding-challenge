e8476b2f201f61bf5a27eb1f92856b4c51e7594e
sunil kumar yadav <123sunilkr@gmail.com>
2025-09-06 00:20:39 +0530

    JS: Abstraction

---
+++ javascript/lld-01-car.js
@@ -0,0 +1,61 @@
+class SportsCar {
+  constructor(brand, modal) {
+    this.brand = brand;
+    this.modal = modal;
+    this.isEnginOn = false;
+    this.currentSpeed = 0;
+    this.currentGear = 0;
+  }
+
+  startEngine() {
+    this.isEnginOn = true;
+    console.log(`${this.brand} ${this.modal} Engine started with a roar!!`);
+  }
+  shiftGear(gear) {
+    if (this.isEnginOn) {
+      this.currentGear = gear;
+      console.log(`${this.brand} ${this.modal} shifted to ${gear} gear`);
+    } else {
+      console.log(`${this.brand} ${this.modal} Engine is not started, please start the engine first`);
+    }
+  }
+
+  accelerate() {
+    if (this.isEnginOn) {
+      this.currentSpeed += 10;
+      console.log(`${this.brand} ${this.modal} accelerated to ${this.currentSpeed} km/h`);
+    } else {
+      console.log(`${this.brand} ${this.modal} Engine is not started, please start the engine first`);
+    }
+  }
+
+  brake() {
+    this.currentSpeed -= 20;
+    if (this.currentSpeed < 0) {
+      this.currentSpeed = 0;
+    }
+    console.log(`${this.brand} ${this.modal} accelerated to ${this.currentSpeed} km/h`);
+  }
+
+  stopEngine() {
+    this.isEnginOn = false;
+    this.currentSpeed = 0;
+    this.currentGear = 0;
+    console.log(`${this.brand} ${this.modal} Engine stopped`);
+  }
+}
+
+const myCar = new SportsCar("BMW", "M3");
+myCar.startEngine();
+myCar.shiftGear(1);
+myCar.accelerate();
+myCar.shiftGear(2);
+myCar.accelerate();
+myCar.accelerate();
+myCar.brake();
+myCar.brake();
+myCar.stopEngine();
+
+// No need to know how startEngine, shiftGear, accelerate, brake, stopEngine works
+// Only need to know how to use the car
+// This is the concept of Abstraction
--- javascript/lld.js
+++ javascript/lld.js
@@ -1,3 +1,79 @@
 // What is LLD?
 
-// for solving isolated problem we use DAA
+// for solving isolated problem we use DSA
+
+// LLD mainly focused on there three 
+
+// 1. Scalability
+// 2. Maintainability
+// 3. Re-useability
+
+
+// What is NOT LLD?
+// HLD
+// Tech Stack, DB, Server scale, cost optimization
+
+// HLD + LLD + DSA = Application
+// HLD: System Architeture
+// LLD: code structure
+// DSA: Tool used by LLD
+
+// If DSA is the brain of an Application then LLD is the skelaton
+
+// OOPs
+
+// Machine language: binary code
+// Assembly language: symbolic code
+// Procedual Programming: Procedure call and return
+// High level language: human readable code
+
+// OOPs: Object Oriented Programming System
+// Object: Real world entity
+// Class: Blueprint of an object
+
+// in real word object intract with each other and with the environment
+// Object will have characteristics and behavior
+// Characteristics are called properties
+// Behavior are called methods
+
+// Example:
+// Object: Car
+// Properties: Color, Brand, Model, Speed (Variables)
+// Methods: Start, Stop, Accelerate, Brake (Methods)
+
+// Object: Human
+// Properties: Name, Age, Gender
+// Methods: Drive, Eat, Sleep, Work, Play
+
+// Object: Dog
+// Properties: Color, Breed, Age, Weight
+// Methods: Bark, Eat, Sleep
+
+// 4 Pillars of OOPs
+// 1. Abstraction
+// 2. Encapsulation
+// 3. Inheritance
+// 4. Polymorphism
+
+// Abstraction:
+// 1. Hide the complex details and show the essential features
+// 2. Example: Car
+// No need to know how startEngine, shiftGear, accelerate, brake, stopEngine works
+// Only need to know how to use the car
+// This is the concept of Abstraction
+
+// Encapsulation:
+// 1. Encapsulate the data and behavior of an object
+// 2. Encapsulation also deal with data hiding
+// 2. Example: Car
+
+// Access modifiers:
+// 1. Public: Accessible from anywhere
+// 2. Private: Accessible only within the class
+// 3. Protected: Accessible within the class and subclasses
+// 4. Default: Accessible within the package
+
+// Example: Car
+// Public: startEngine, shiftGear, accelerate, brake, stopEngine
+// Private: color, brand, model, speed
+// Protected: currentGear
